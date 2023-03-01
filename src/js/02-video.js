409b02588a1d41ede301d5c06982f357e126ca79
IhorC <igorchua@gmail.com>
2023-03-01 20:58:06 +0200

    task 3 finished

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,10 +4,16 @@ const throttle = require('lodash.throttle');
 // const iframePlayer = new Vimeo.Player(iframe);
 const idPlayer = new Player('vimeo-player');
 
+if(localStorage.getItem("videoplayer-current-time") !== null){
+    idPlayer.setCurrentTime(getTime().seconds).then(function(seconds) {
+        // seconds = the actual time that the player seeked to
+    });
+}
+
 idPlayer.on('timeupdate', throttle((data)=>{
     try{
         const serializedTime = JSON.stringify({seconds : Math.floor(data.seconds)})
-        localStorage.setItem('timer', serializedTime);
+        localStorage.setItem("videoplayer-current-time", serializedTime);
     } catch (error) {
         console.log("Get state error: ", error.message);
     }
@@ -15,13 +21,9 @@ idPlayer.on('timeupdate', throttle((data)=>{
 
 function getTime(){
     try {
-        const serializedTime = localStorage.getItem('timer');
+        const serializedTime = localStorage.getItem("videoplayer-current-time");
         return serializedTime === null ? undefined : JSON.parse(serializedTime);
     } catch (error) {
         console.log('Set current time error: ', error.message);
     }
 }
-
-idPlayer.setCurrentTime(getTime().seconds).then(function(seconds) {
-    // seconds = the actual time that the player seeked to
-});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+const form = document.querySelector('.feedback-form');
+const throttle = require('lodash.throttle');
 
+form.addEventListener('submit', preventDefault);
+form.addEventListener('submit', throttle(saveInputs, 500, {"trailing": false}));
+
+if(localStorage.getItem('feedback-form-state') !== null){
+    try {
+        const serializedInputs = JSON.parse(localStorage.getItem("feedback-form-state"));
+
+        document.querySelector('[name="email"]').value = serializedInputs.email;
+        document.querySelector('[name="message"]').value = serializedInputs.message;
+
+    } catch (error) {
+        console.log(error.message);
+    }
+} 
+
+function saveInputs(event){
+    localStorage.removeItem("feedback-form-state");
+    const {
+        elements: {email, message}
+    } = event.target;
+  
+    try {
+        const serializedInputs = JSON.stringify({email: email.value, message: message.value});
+        localStorage.setItem("feedback-form-state", serializedInputs);
+    } catch (error){
+        console.log(error.message);
+    }
+ 
+    console.log({email: email.value, message: message.value})
+    // localStorage.clear();
+}
+console.log("local storage: ", localStorage.getItem("feedback-form-state"));
+
+function preventDefault(event){
+    event.preventDefault();
+}
