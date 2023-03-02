1864b6d99aa298443e778d8c9073891ea089bbc9
IhorC <igorchua@gmail.com>
2023-03-02 16:28:34 +0200

    task 3 fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,10 +1,9 @@
 const throttle = require('lodash.throttle');
 
 const form = document.querySelector('.feedback-form');
-const email = document.querySelector('[name="email"]');
-const message = document.querySelector('[name="message"]');
+const saveInputsThrottle = throttle(saveInputs, 500);
 
-form.addEventListener('input', throttle(saveInputs, 500));
+form.addEventListener('input', saveInputsThrottle);
 form.addEventListener('submit', clearInputs);
 
 if(localStorage.getItem('feedback-form-state') !== null){
@@ -20,13 +19,22 @@ if(localStorage.getItem('feedback-form-state') !== null){
     console.log('getItem ', localStorage.getItem('feedback-form-state'))
 } 
 
-function saveInputs(){
-    try {
-        const serializedInputs = JSON.stringify({email: email.value, message: message.value});
-        localStorage.setItem("feedback-form-state", serializedInputs);
-    } catch (error){
-        console.log(error.message);
+function saveInputs(event){
+    if(event.currentTarget?.elements) {;
+        
+        const {
+            elements: {email, message}
+        } = event.currentTarget;
+        try {
+            const serializedInputs = JSON.stringify({email: email.value, message: message.value});
+            localStorage.setItem("feedback-form-state", serializedInputs);
+        } catch (error){
+            console.log(error.message);
+        }
+    // console.log(event.currentTarget.elements.message.value)
+
     }
+
 }
 
 function clearInputs(event){
