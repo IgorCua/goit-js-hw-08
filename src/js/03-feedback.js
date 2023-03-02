e25c2cc8d7a598d5756fa002da8bdd4b8b456b68
IhorC <igorchua@gmail.com>
2023-03-02 18:56:21 +0200

    task 3 fixed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,41 +2,49 @@ const throttle = require('lodash.throttle');
 
 const form = document.querySelector('.feedback-form');
 const saveInputsThrottle = throttle(saveInputs, 500);
+let formEmail = document.querySelector('[name="email"]');
+let formMessage = document.querySelector('[name="message"]');
+let globalEmail = '';
+let globalMessage = '';
 
 form.addEventListener('input', saveInputsThrottle);
 form.addEventListener('submit', clearInputs);
 
+// getting localStorage values
 if(localStorage.getItem('feedback-form-state') !== null){
     try {
         const serializedInputs = JSON.parse(localStorage.getItem("feedback-form-state"));
+        console.log(serializedInputs);
+        
+        formEmail.value = serializedInputs.email;
+        formMessage.value = serializedInputs.message;
 
-        email.value = serializedInputs.email;
-        message.value = serializedInputs.message;
+        globalEmail = serializedInputs.email;
+        globalMessage = serializedInputs.message;
 
     } catch (error) {
-        console.log(error.message);
+        console.log("getItem error: ", error.message);
     }
     console.log('getItem ', localStorage.getItem('feedback-form-state'))
 } 
 
+//saving input values to local storage
 function saveInputs(event){
-    if(event.currentTarget?.elements) {
-        
-        const {
-            elements: {email, message}
-        } = event.currentTarget;
-        try {
-            const serializedInputs = JSON.stringify({email: email.value, message: message.value});
-            localStorage.setItem("feedback-form-state", serializedInputs);
-        } catch (error){
-            console.log(error.message);
-        }
-    // console.log(event.currentTarget.elements.message.value)
+    console.log('target: ', event)
 
+    event.target.name === 'email' ? globalEmail = event.target.value 
+                                    : globalMessage = event.target.value;            
+   
+    try {
+        const serializedInputs = JSON.stringify({'email': globalEmail, 'message': globalMessage});
+        localStorage.setItem("feedback-form-state", serializedInputs);
+    } catch (error){
+        console.log(error.message);
     }
 
 }
 
+//clearing inputs and local storage
 function clearInputs(event){
     event.preventDefault();
     const {
@@ -51,6 +59,8 @@ function clearInputs(event){
 
     email.value = '';
     message.value = '';
+    globalEmail = '';
+    globalMessage = '';
 
     localStorage.removeItem("feedback-form-state");
 }
